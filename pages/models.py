dc799e85a2c4de8a9853da6af32d0994b26623fa
gingerjoos <gingerjoos@gmail.com>
2012-07-05 09:43:58 +0530

    initial page model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=255,unique=True)
+    body = models.TextField()
+    is_published = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(User)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    @models.permalink
+    def get_absolute_url(self):
+            return self.slug
+
+    def __unicode__(self):
+        return self.title
